3de33555a2189ce6f5336461cc087aa80fc230eb
salvatore <salvatore.sorrentino@live.com>
2023-01-25 16:48:37 +0100

    customer controller

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using demo.core.interfaces;
+using demo.core.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IWelcome welcome;
+
+        public CustomersController(IWelcome welcome)
+        {
+            this.welcome = welcome;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(ExtractData());
+        }
+
+        private  List<Customer> ExtractData()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Mario Rossi " + welcome.SayHello(), City = "Naples" },
+                new Customer { Id = 2, Name = "Luigi Bianchi", City = "Milano" }
+            };
+        }
+    }
+}
